321f60f906d5cf12ab14fe0ad828a8c51b24fce1
unknown <christopherbonime@gmail.com>
2022-12-14 11:40:24 -0500

    added fetch youtube trailer function with proper keywords to display correct trailer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var movieDetailsEl = document.querySelector("#movie-details");
 var imdbBtnEl = document.querySelector("#IMDB-btn");
 var youtubeVideoEl = document.querySelector("#youtube-video");
 var apiKey = "f026d548";
+var youtubeApiKey = "AIzaSyCn90WcyhMlfORhRQMjSWg1jSozf4ZlfF4";
+var youtubeApiKey2 = "AIzaSyAT9a8lxxa7X2nHaEXa7LhPl2IDCLTekyM"
 
 var displayMovieDetails = function (search) {
     movieDetailsEl.innerHTML = null;
@@ -23,9 +25,21 @@ var displayMovieDetails = function (search) {
     movieDetailsEl.append(movieTitle, moviePoster, mediaType, releaseDate);
 };
 
+var youtubeDisplay = function (search) {
+    youtubeVideoEl.innerHTML = null;
+    var youtubeTrailerTitle = document.createElement("h3");
+    youtubeTrailerTitle.className = "movie-title text-center my-4"
+    youtubeTrailerTitle.textContent = search.Search[0].Title + " " + search.Search[0].Year;
+    var youtubeVideo = document.createElement("iframe");
+    youtubeVideo.className = `border solid 4px border-light” width=“560" height=“315” src=“${youtubeUrl}” frameborder=“0” allow=“accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture” allowfullscreen`;
+    youtubeVideoEl.appendChild(youtubeTrailerTitle);
+    youtubeTrailerTitle.append(youtubeVideo);
+}
+
 var fetchTrailer = function(search) {
-    var searchQuery = search.Search[0].Title + " " + search.Search[0].year;
-    var youtubeUrl = "";
+    var searchQuery = search.Search[0].Title + " " + search.Search[0].Year;
+    console.log(searchQuery)
+    var youtubeUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchQuery}${"trailer"}&key=${youtubeApiKey2}`;
     fetch(youtubeUrl)
         .then(function(res){
             return res.json();
@@ -36,7 +50,7 @@ var fetchTrailer = function(search) {
         .catch(function(err){
             console.log(err);
         });
-}
+};
 
 var fetchResults = function(movieInputEl) {
     var apiUrl = `http://www.omdbapi.com/?s=${movieInputEl}&apikey=${apiKey}`;
@@ -47,8 +61,7 @@ var fetchResults = function(movieInputEl) {
     })
     .then(function (data) {
         displayMovieDetails(data);
-        // fetchTrailer(data);
-        imdbBtnEl.classList.remove("hidden");
+        fetchTrailer(data);
         console.log(data);
     })
     .catch(function (error) {
@@ -59,56 +72,21 @@ var fetchResults = function(movieInputEl) {
 var handleSearch = function() {
     var search = movieInputEl.value.trim();
     fetchResults(search);
-    console.log(search); 
+    console.log(search);
 };
 
-// var youtubeApiKey = "AIzaSyCn90WcyhMlfORhRQMjSWg1jSozf4ZlfF4";
 // var youtubeApiKey2 = "AIzaSyDWmXhjEBiBf76Wf4dj-E5_sc6KjDhagYU";
 // var youtubeSearchCri = movieInputEl + releaseDate;
 
-// var youtubeApiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${youtubeSearchCri}&key=${youtubeApiKey}`
 
-// 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=[YOUR_API_KEY]'
-
-var youtubeApiUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=StarWarsEpisodeIV-ANewHope-movie-review&key=AIzaSyCn90WcyhMlfORhRQMjSWg1jSozf4ZlfF4';
-
-fetch(youtubeApiUrl)
-.then(res => res.json())
-.then(data => console.log(data));
-
-var getVideos = function () {
-    var youtubeUrl = `http://www.omdbapi.com/?s=${movieInputEl}&apikey=${apiKey}`
-}
-// var displayYoutubeVideo = function (items) {
-    //     youtubeVideoEl.innerHTML = null;
-    //     var youtubePlayer = document.createElement("iframe");
-    //     youtubePlayer.className = "width='560', height='315' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen"
-    //     youtubePlayer.textContent = 
-    
-    
-    //   };
-    
-    //   var fetchYoutubeResults = function(movieInputEl) {
-        //     var youtubeApiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${movieInputEl}&key=${youtubeApiKey}`
-        
-        //     fetch(youtubeApiUrl)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (data) {
-    //             displayYoutubeVideo(data);
-    //             console.log(data);
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         });
-    //     };
-        
-    //     var handleYoutubeSearch = function() {
-    //         var search = movieInputEl.value.trim();
-    //         fetchYoutubeResults(search);
-    //         console.log(search); 
-    //     };
+// .then(function (response) {
+//     $(“.video”).empty()
+//     var videoID = (response.items[“0"].id.videoId)
+//    const videoURL = “https://www.youtube.com/embed/” + videoID
+//    $(“.video”).append(`
+//        <iframe class=“border solid 4px border-light” width=“560" height=“315” src=“${videoURL}” frameborder=“0” allow=“accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture” allowfullscreen></iframe>
+//        `)
+//      });
 
 searchBtnEl.addEventListener("click", handleSearch, function(event) {
     event.preventDefault();
